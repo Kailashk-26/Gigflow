343bb04b5b940a7a827b99df2804aafe3c98cbc5
kailash <kailashofficial26@gmail.com>
2026-01-15 21:13:22 +0530

    logout issue fixed

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -121,6 +121,8 @@ export const logoutUser = async (req, res) => {
   try {
     res.cookie("token", "", {
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       expires: new Date(0),
     });
 
